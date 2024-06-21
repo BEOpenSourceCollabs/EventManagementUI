78c4b85169a62eed4a9ae72a9d11f4e5de261194
dimitriost1 <46889040+dimitriost1@users.noreply.github.com>
2024-06-21 22:55:03 +0300

    sign up button-fix (#6)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-
 function Header() {
   return (
     <div className="navbar bg-base-100">
@@ -7,14 +6,20 @@ function Header() {
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
-          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
         </div>
       </div>
 
       <div className="navbar-end flex gap-4">
         <a>Log In</a>
-        <div className="bg-primary text-primary-content">
-          <button className="btn btn-ghost">Sign Up</button>
+        <div>
+          <button className="btn btn-ghost  bg-primary text-primary-content">
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
